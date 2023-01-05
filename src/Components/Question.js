20774d3be142cab32b99bfd48d926df2109870b3
Andrew <aforest41@yahoo.com>
2023-01-05 15:26:04 -0800

    Answers can be selected

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,6 +1,26 @@
-function Question({character, question, answers, correctAnswer}){
-    const handleChange = () => {
+import { useState } from "react"
 
+function Question({character, question, answers, correctAnswer, handleSubmit}){
+    const [selected, setSelected] = useState(answers.answer1);
+
+    const handleChange = (e) => {
+        switch (e.target.name) {
+            case "option1":
+                setSelected(answers.answer1)
+                break;
+            case "option2":
+                setSelected(answers.answer2)
+                break;
+            case "option3":
+                setSelected(answers.answer3)
+                break;
+            case "option4":
+                setSelected(answers.answer4)
+                break;
+            default:
+                setSelected(answers.answer1)
+                break;
+        }
     }
 
     return(
@@ -14,7 +34,7 @@ function Question({character, question, answers, correctAnswer}){
                         type="radio"
                         name="option1"
                         value="male"
-                        checked={true}
+                        checked={selected===answers.answer1}
                         onChange={handleChange}
                         />
                         {" "}
@@ -25,7 +45,7 @@ function Question({character, question, answers, correctAnswer}){
                         type="radio"
                         name="option2"
                         value="female"
-                        checked={false}
+                        checked={selected===answers.answer2}
                         onChange={handleChange}
                         />
                         {" "}
@@ -36,7 +56,7 @@ function Question({character, question, answers, correctAnswer}){
                         type="radio"
                         name="option3"
                         value="male"
-                        checked={false}
+                        checked={selected===answers.answer3}
                         onChange={handleChange}
                         />
                         {" "}
@@ -47,7 +67,7 @@ function Question({character, question, answers, correctAnswer}){
                         type="radio"
                         name="option4"
                         value="female"
-                        checked={false}
+                        checked={selected===answers.answer4}
                         onChange={handleChange}
                         />
                         {" "}
